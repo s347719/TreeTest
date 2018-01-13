bdc85967efb62680229e60357574fd599aefc388
578076417@qq.com <h347719>
2018-01-13 16:03:44 +0800

    Initial commit

---
+++ app/src/main/java/com/example/yineng/treetest/TestActivity.java
@@ -0,0 +1,15 @@
+package com.example.yineng.treetest;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TestActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_test);
+
+
+    }
+}
